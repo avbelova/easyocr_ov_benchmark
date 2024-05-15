f52f8300d83b992b9931b5c96831825a3f36ea10
avbelova <anna.gubenkova@intel.com>
2024-05-15 16:18:16 +0200

    Add multi-language benchmarking support

--- easyocr_ov_benchmark.py
+++ easyocr_ov_benchmark.py
@@ -20,18 +20,18 @@ def benchmark(reader, img, niter):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("image", help="Path to input image")
-    parser.add_argument("language", help="Language in easyOCR format: en for English, hi for Hindi, etc. ")
-    parser.add_argument("-d", "--device_ov", help="Inference device in OpenVINO format", default="ov_cpu")
-    parser.add_argument("-n", "--iterations_number", help="Number of benchmarking iterations", type=int, default=10)
+    parser.add_argument("image", help = "Path to input image")
+    parser.add_argument("language", help = "Language in easyOCR format: en for English, hi for Hindi, etc. ", nargs = '+')
+    parser.add_argument("-d", "--device_ov", help = "Inference device in OpenVINO format", default = "ov_cpu")
+    parser.add_argument("-n", "--iterations_number", help = "Number of benchmarking iterations", type = int, default = 10)
     args = parser.parse_args()
     img = cv2.imread(args.image)
     language = args.language
     ov_device = args.device_ov
     niter = args.iterations_number
-    reader_pt = easyocr.Reader([language],"cpu")
+    reader_pt = easyocr.Reader(language,"cpu")
     latency_pt = benchmark(reader_pt, img, niter)
-    reader_ov = easyocr.Reader([language], ov_device)
+    reader_ov = easyocr.Reader(language, ov_device)
     latency_ov = benchmark(reader_ov, img, niter)
 
     print("------------------------------------------------------")
