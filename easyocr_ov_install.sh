efaba52e3b4807d0267f0df998bb6609d7778602
Gubenkova <anna.gubenkova@intel.com>
2024-08-01 18:07:10 +0200

    changing the installation process

--- easyocr_ov_install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/avbelova/EasyOCR.git
-git checkout feature/openvino-backend
-pip install wheel
-cd EasyOCR/
-python setup.py bdist_wheel
-pip install dist/easyocr-1.7.1-py3-none-any.whl
-echo "EasyOCR OpenVINO is successfully installed."
